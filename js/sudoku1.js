0ff74a1d7d832e3686c7279b9719e42894239160
Matt <matt.flaig@web.de>
2014-06-05 18:01:56 +0200

    struggling with fourth line

--- js/sudoku1.js
+++ js/sudoku1.js
@@ -4,8 +4,13 @@ var firstBlock = [];
 var secondBlock = [];
 var thirdBlock = [];
 var missingElements = [];
+var columnArray = [[],[],[],[],[],[],[],[],[]];
+
+
+
 // var missingElements = [];
 // var doubledElements = [];
+
   function manageComputation(){
   	sudokuArray = [], blockArray = [], missingElements = [];
     firstBlock = [], secondBlock = [], thirdBlock = [];
@@ -13,6 +18,8 @@ var missingElements = [];
     getBlock();
     manageBlocks();
     changeElements(arrayIndex=0);
+    getColumns();
+    continueBlocks();
   }
 
   function makeSudokuArray(){
@@ -106,10 +113,11 @@ var missingElements = [];
           marker -= 1;
         }
       }
-      if(marker == 9){
+      if(marker == array.length){
         checkArray.push(i);
       }
     }
+    //if(signal){alert("checkArray: " + checkArray);}
     if(checkArray.length > 0){return checkArray;}
   }
 
@@ -123,6 +131,7 @@ var missingElements = [];
         else {doubleChoices.push(array[i]);}
     }
     if(doubleChoices.length > 0){return doubleChoices;}
+    else if(!doubled){return uniqueChoices;}
     else{return undefined;}
   }
 
@@ -195,3 +204,153 @@ function alternativeChange(block, check, doubledElement){
   var doubledNextIndex = rowNext.indexOf(doubledElement[0]);
   manageChange(checkNextIndex, check, doubledNextIndex, doubledElement, rowNextIndex, rowNext);
 }
+
+
+function getColumns(){
+  columnArray = [[],[],[],[],[],[],[],[],[]];
+  for(var i=0;i<sudokuArray.length;++i){
+    var getColumnArray = sudokuArray[i];
+    pushColumns(getColumnArray);
+  }
+}
+
+
+  function pushColumns(getColumnArray){
+    for(var j=0; j<getColumnArray.length; ++j){
+      var columnElement = getColumnArray[j];
+      for(k=0;k<9;++k){
+        if(j==k){
+          columnArray[k].push(columnElement);
+        }
+      }
+    }
+  }
+
+
+/*BLOCKS 4 AND HIGHER*/
+
+// function getNewLine(counter){
+//   var array = [1,2,3,4,5,6,7,8,9];
+//   var newArray = [];
+//   while(array.length != 0){
+//     var arrayIndex = Math.floor(Math.random() * array.length);
+//     var arrayElement = array[arrayIndex];
+//     newArray.push(arrayElement);
+//     array.splice(arrayIndex, 1);
+//   }
+//   //return newArray
+//   //alert(newArray);
+//   var lineArray = compareForDoubles(newArray);
+//   if(lineArray != undefined){return lineArray;}
+// }
+
+// function compareForDoubles(newArray){
+// var doubleCounter = 0;
+//   for(var i = 0; i<columnArray.length;++i){
+//     var column = columnArray[i];
+//     //for(var j=0;j<newArray.length; ++j){
+//     var element = newArray[i];
+//     var elementArray = [];
+//     elementArray.push(element);
+//     var composedArray = elementArray.concat(column);
+//     //alert("column: " + column);
+//     //alert("composed: " + composedArray);
+//     var checkDouble = getUniqueOrDouble(composedArray, doubled=1);
+//     //alert(checkDouble);
+//     if(checkDouble != undefined){
+//       doubleCounter += 1;
+//     }
+//   }
+//   if (doubleCounter < 3){return newArray;}
+//   else {getNewLine();}
+// }
+
+// // var columnComparison = getComparisonArray(counter);
+// // var possibleNumbers = checkAgainstNine(columnComparison);
+// // var choiceElement = randomChoice(possibleNumbers);
+
+// //     if(possibleNumbers.length > 0) {
+// //       if(k>0){ 
+// //         possibleNumbers = manageNewLine(newLine, possibleNumbers);
+// //         var choiceElement = randomChoice(possibleNumbers);
+// //         alert("choice: " + choiceElement)
+// //         if(!isNaN(parseInt(choiceElement))){
+// //           newLine.push(choiceElement);
+// //         }
+// //       }
+// //       else{
+// //         // var choiceElement = randomChoice(possibleNumbers);
+// //         // newLine.push(choiceElement);
+// //         getNewLine(columnNumber=0);
+// //       }
+// //     }
+// //   }
+// //   if(newLine.length == 9){
+// //     alert("return: " + newLine);
+// //     return newLine;
+// //   }
+// // }
+
+// // function manageNewLine(newLine, possibleNumbers){
+// //   for(var i=0; i<newLine.length; ++i){
+// //     var newLineElement = newLine[i];
+// //     //alert("newLineElement: " + newLineElement);
+
+// //     for(var j=0; j<possibleNumbers.length; ++j){
+// //       var possibleElement = possibleNumbers[j];
+// //       if(newLineElement == possibleElement){
+// //         possibleNumbers.splice(j, 1);
+// //       }
+// //     }
+// //   }
+// //   return possibleNumbers;
+// // }
+
+// // function randomChoice(possibleNumbers){
+// // var choiceIndex = Math.floor(Math.random() * possibleNumbers.length);
+// // var choiceElement = possibleNumbers[choiceIndex];
+// // return choiceElement;
+// // }
+
+// function continueBlocks(){
+//   //var newLine = [];
+//   var blockLine = getNewLine();
+//   var ausgabe = "ausgabe" + 4;
+//   //sudokuArray.push(blockLine);
+//   loadView(ausgabe, blockLine);
+// }
+
+
+
+// // function getComparisonArray(counter){
+// //     var comparisonArray = [];
+// //     for(var i=0;i<sudokuArray.length;++i){
+// //       var row = sudokuArray[i];
+
+// //       for(var j=0;j<row.length;++j){
+// //         var element = row[j];
+// //         if(j==counter){comparisonArray.push(element);}
+// //       }
+// //     }
+// //     return comparisonArray;
+// // }
+
+function getColumns(){
+  columnArray = [[],[],[],[],[],[],[],[],[]];
+  for(var i=0;i<sudokuArray.length;++i){
+    var getColumnArray = sudokuArray[i];
+    pushColumns(getColumnArray);
+  }
+}
+
+
+  function pushColumns(getColumnArray){
+    for(var j=0; j<getColumnArray.length; ++j){
+      var columnElement = getColumnArray[j];
+      for(k=0;k<9;++k){
+        if(j==k){
+          columnArray[k].push(columnElement);
+        }
+      }
+    }
+  }
