8af4607154cf65bd2ad6dded07a79fe22443b6bb
MattFlaig <matt.flaig@web.de>
2014-06-25 05:36:57 +0200

    comments

--- js/sudoku2.js
+++ js/sudoku2.js
@@ -1,3 +1,8 @@
+/*GLOBAL VARIABLES*/
+
+//computing Round is set to 1 in the beginning, will be augmented during computation
+//columns, rows, and blocks arrays are set to empty, as well as the comparison arrays (checkArray, fillArray)
+//variable restart is set to false
 var computingRound = 1;
 var columns = [];
 var rows = [];
@@ -83,14 +88,12 @@ function computeFourthBlock(){
   var blockNumber = 4;
   var fourthBlock = getNewBlockFromGivenBlock(columns);
   manageBlocks(fourthBlock, blockNumber);
-  //createSudokuTable();
 }
 
 function computeFifthBlock(){
   var blockNumber = 5;
   var fifthBlock = getCentralBlock();
   manageBlocks(fifthBlock, blockNumber);
-  createSudokuTable();
 }
 
 function computeSixthBlock(){
@@ -136,7 +139,6 @@ function manageBlocks(block, blockNumber){
   pushToRows(block, blockNumber);
   pushToColumns(block, blockNumber);
   computingRound += 1;
-  //visualizeRows();
   if(blockNumber<7){
     if(blockNumber < 4){
       pushBlockRow(block, fillArray, 0, 1, 2);
@@ -168,6 +170,8 @@ function createSudokuTable(){
 
 /*2nd, 4th AND 5th BLOCK FUNCTIONS*/
 
+//function getNewBlockFromGivenBlock(array):
+//to compute a new block from an array of given elements as parameter
 function getNewBlockFromGivenBlock(array){
   var block = [];
   var flag = true;
@@ -186,6 +190,12 @@ function getNewBlockFromGivenBlock(array){
   return block;
 }
 
+//function getNextElement(block, array, indexNumber):
+//determining next element by first getting a lineNumber
+//to know which line to compare with, then checking which numbers 
+//are still possible to choose and lastly choose one by random from these
+//Note: if rows are given as array, the progression of the iterator number is  
+//upwards -> downwards not left -> right and vice versa
 function getNextElement(block, array, indexNumber){
   var lineNumber = getLineNumber(indexNumber);
   var stillPossible = getStillPossible(block, array, lineNumber);
@@ -193,6 +203,8 @@ function getNextElement(block, array, indexNumber){
   return nextElement;
 }
 
+//function getLineNumber(indexNumber):
+//computing lineNumber depending on index
 function getLineNumber(indexNumber){
   if(computingRound<5){
     if(indexNumber==0){var lineNumber = 2;}
@@ -205,6 +217,8 @@ function getLineNumber(indexNumber){
   return lineNumber;
 }
 
+//function getStillPossible(block, array, lineNumber):
+//to get the still possible Elements to choose from
 function getStillPossible(block, array, lineNumber){
   var stillPossible = checkAgainstNine(array[lineNumber]);
   if(block != []){
@@ -213,6 +227,8 @@ function getStillPossible(block, array, lineNumber){
   return stillPossible;
 }
 
+//function updateStillThere(updateArray, stillThere):
+//to update the still possible elements and subtract those which were already chosen
 function updateStillThere(updateArray, stillThere){
   for(var i = 0; i<updateArray.length;++i){
     var arrayElement = updateArray[i];
@@ -226,6 +242,10 @@ function updateStillThere(updateArray, stillThere){
   return stillThere;
 }
 
+//function getCentralBlock():
+//computes 5th block in a loop which runs until there are no doubles 
+//(computed in counter-function below)in the same line/column and no triples 
+//in a different one
 function getCentralBlock(){
   var marker = true;
   while(marker){
@@ -237,6 +257,9 @@ function getCentralBlock(){
   return columnBlock;
 }
 
+//function getCounter(columnBlock):
+//computes counter (if a non-doubled element occurs in comparison, 1 is added)
+//for getCentralBlock-function
 function getCounter(columnBlock){
   var counter = 0;
   for(var i=0;i<columnBlock.length;++i){
@@ -253,10 +276,15 @@ function getCounter(columnBlock){
   return counter;
 }
 
+//function scrutinizeBlock(block):
+//checks if there are the same three elements (called "triples" here)
+//in any row of the new block as in the block before
+//Although a block can be valid then it is considered ugly (seems not "random" enough)
+//and therefore dismissed
 function scrutinizeBlock(block){
   var scrutinizer = "";
   var compareArray = [fillArray[3], fillArray[4], fillArray[5]];
-  var lines = getSubarraysFromRows(block);
+  var lines = getSubarrays(block);
   var triples = searchForTriples(compareArray, lines);
   if(triples != undefined){
     scrutinizer = "notcorrect";
@@ -271,6 +299,10 @@ function scrutinizeBlock(block){
 
 /*FILL MISSING ELEMENTS FUNCTIONS FOR BLOCKS 3 AND 6-9*/
 
+//function fillWithMissingElements(first, second, third, array):
+//fills missing elements when two blocks in a row/column are already computed
+//takes the indexes of the lines which should be filled and either rows or 
+//columns as array-parameter
 function fillWithMissingElements(first, second, third, array){
   var newBlock = [];
   var firstLine = checkAgainstNine(array[first]);
@@ -282,10 +314,13 @@ function fillWithMissingElements(first, second, third, array){
   return newBlock;
 }
 
+//function mixFilledArray(block):
+//to mix up the filled elements for more random flavour
+//note: the mix is not executed on the whole block but only
+//on the respective three filled elements of a row/column  
 function mixFilledArray(block){
   var newArray = [];
-  
-  var filledSubarrays = getSubarraysFromRows(block);
+  var filledSubarrays = getSubarrays(block);
   for(var i=0;i<filledSubarrays.length;++i){
     var subArray = filledSubarrays[i];
     var newSubarray = findRandomArray(subArray);
@@ -299,10 +334,17 @@ function mixFilledArray(block){
 
 /*6th AND 8th BLOCK FUNCTIONS*/
 
+//function chooseFromFilledElements(block, compareLines):
+//in the higher blocks it is not enough to just fill the missing
+//elements and mix them - you also have to make sure that they
+//are compared to the elements of another block which is taken care of in this function
+//(block6 is filled based on information from blocks 4 and 5, but also has to 
+//match the column-elements of block 3 directly above)
+//As parameters are given the just filled block and either rows or columns, from which 
+//the lines the other block is compared with are taken
 function chooseFromFilledElements(block, compareLines){
-
   var checkLines = [compareLines[6], compareLines[7], compareLines[8]];
-  var newlyFilledArray = getSubarraysFromRows(block);
+  var newlyFilledArray = getSubarrays(block);
   var checkForTriples = searchForTriples(checkLines, newlyFilledArray);
   if(checkForTriples == undefined){
     var array = [];
@@ -316,6 +358,9 @@ function chooseFromFilledElements(block, compareLines){
   else{restart = true;}
 }
 
+//function searchForTriples(checkLines, newlyFilledArray):
+//to check if three elements of a line are identical with those
+//of a neighbor block's 
 function searchForTriples(checkLines, newlyFilledArray){
   var tripled=0;
   for(var i=0;i<newlyFilledArray.length;++i){
@@ -333,7 +378,12 @@ function searchForTriples(checkLines, newlyFilledArray){
   else{return undefined;}
 }
 
-
+//function getFilledLine(filledElement, checkLines):
+//this function first computes a random sequence ("checkElements") 
+//from a newly filled line ("filled element")and then checks if each 
+//element of this line has doubles with the respective checkLine from
+//the block to compare with
+//The loop runs as long as there are doubled elements
 function getFilledLine(filledElement, checkLines){
   var marker = true;
   while(marker){
@@ -356,10 +406,12 @@ function getFilledLine(filledElement, checkLines){
 
 /*9th BLOCK FUNCTIONS*/
 
+//function getCommonNumbers(fillBlockColumns, fillBlockRows):
+//to find the overlap between filled columns and rows for last block
 function getCommonNumbers(fillBlockColumns, fillBlockRows){
   var newArray = [];
-  var allColumns = getSubarraysFromColumns(fillBlockColumns);
-  var allRows = getSubarraysFromRows(fillBlockRows);
+  var allColumns = getSubarrays(fillBlockColumns);
+  var allRows = getSubarrays(fillBlockRows);
   for(var i=0;i<9;++i){
     var concatArray = getConcatForLastBlock(allColumns, allRows, i);
     var getDouble = getUniqueOrDouble(concatArray, doubled=1);
@@ -372,7 +424,8 @@ function getCommonNumbers(fillBlockColumns, fillBlockRows){
   if(newArray.length == 9){return newArray;}
 }
 
-
+//function getConcatForLastBlock(allColumns, allRows, i):
+//to mange the concated array for the last block
 function getConcatForLastBlock(allColumns, allRows, i){
   if(i<3){
       var row = allRows[0], column = allColumns[i];
@@ -390,6 +443,8 @@ function getConcatForLastBlock(allColumns, allRows, i){
 
 /*GENERAL FUNCTIONS USED FOR ALL BLOCKS*/
 
+//function prepareRows(prepareArray):
+//to get the global rows array ready 
 function prepareRows(prepareArray){
   for(var i=0;i<9;++i){
     var emptyArray = [];
@@ -397,6 +452,8 @@ function prepareRows(prepareArray){
   }
 }
 
+//function prepareColumns():
+//to get the global columns array ready 
 function prepareColumns(){
   for(var i=0;i<9;++i){
     var emptyArray = [];
@@ -404,28 +461,26 @@ function prepareColumns(){
   }
 }
 
-function getSubarraysFromRows(fillBlockRows){
-  var row1 = [fillBlockRows[0], fillBlockRows[1], fillBlockRows[2]];
-  var row2 = [fillBlockRows[3], fillBlockRows[4], fillBlockRows[5]];
-  var row3 = [fillBlockRows[6], fillBlockRows[7], fillBlockRows[8]];
-  var allRows = [row1, row2, row3];
-  return allRows;
+//function getSubarrays(fillBlock):
+//to get subarrays from a given block
+function getSubarrays(fillBlock){
+  var subArray1 = [fillBlock[0], fillBlock[1], fillBlock[2]];
+  var subArray2 = [fillBlock[3], fillBlock[4], fillBlock[5]];
+  var subArray3 = [fillBlock[6], fillBlock[7], fillBlock[8]];
+  var allSubarrays = [subArray1, subArray2, subArray3];
+  return allSubarrays;
 }
 
-function getSubarraysFromColumns(fillBlockColumns){
-  var column1 = [fillBlockColumns[0], fillBlockColumns[1], fillBlockColumns[2]];
-  var column2 = [fillBlockColumns[3], fillBlockColumns[4], fillBlockColumns[5]];
-  var column3 = [fillBlockColumns[6], fillBlockColumns[7], fillBlockColumns[8]];
-  var allColumns = [column1, column2, column3];
-  return allColumns;
-}
-
+//function getOneByRandom(array):
+//get one random element from a given array
 function getOneByRandom(array){
   var arrayIndex = Math.floor(Math.random() * array.length);
   var arrayElement = array[arrayIndex];
   return arrayElement;
 }
 
+//function findRandomArray(array):
+//get a shuffled array from a given one
 function findRandomArray(array){
   var newArray = [], emptyArray = [];
   var deleteArray = array.concat(emptyArray);
@@ -439,6 +494,9 @@ function findRandomArray(array){
   return newArray;
 }
 
+//function getUniqueOrDouble(array, doubled):
+//get either the unique elements of a given array or the doubled ones
+//(if parameter doubled is there) returned
 function getUniqueOrDouble(array, doubled) {
   var hash = {}, uniqueChoices = [], doubleChoices = [];
   for ( var i = 0; i < array.length; ++i ) {
@@ -453,6 +511,9 @@ function getUniqueOrDouble(array, doubled) {
   else{return undefined;}
 }
 
+//function checkAgainstNine(array):
+//takes an array of numbers from 1-9 and returns all numbers
+//which are not part of the given array 
 function checkAgainstNine(array){
   var checkNineArray = [];
   for(var i=1; i<10; ++i){
@@ -469,6 +530,12 @@ function checkAgainstNine(array){
   if(checkNineArray.length > 0){return checkNineArray;}
 }
 
+//function transformToBlock(columnBlock):
+//transforms a columnBlock where the progression is from upwards
+//to downwards to a rowBlock where it is from left to right
+//1,4,7                     1,2,3
+//2,5,8  is transformed to  4,5,6
+//3,6,9                     7,8,9
 function transformToBlock(columnBlock){
   var first = [], second = [], third = [];
   for(var i=0;i<columnBlock.length;++i){
@@ -481,6 +548,8 @@ function transformToBlock(columnBlock){
   return transformedBlock;
 }
 
+//function pushToColumns(array, blockNumber):
+//manages push to columns depending on blockNumber
 function pushToColumns(array, blockNumber){
   if(blockNumber == 1 || blockNumber == 4 || blockNumber == 7){
     pushBlockColumn(array, 0, 1, 2);
@@ -493,6 +562,8 @@ function pushToColumns(array, blockNumber){
   }
 }
 
+//function pushBlockColumn(array, column1, column2, column3):
+//actual push of given row-block to columns 
 function pushBlockColumn(array, column1, column2, column3){
   for(var i=0;i<array.length;++i){
     var arrayElement = array[i];
@@ -505,6 +576,8 @@ function pushBlockColumn(array, column1, column2, column3){
   }
 }
 
+//function pushToRows(array, blockNumber):
+//manages push to rows depending on blockNumber
 function pushToRows(array, blockNumber){
   if(parseInt(blockNumber) <= 3 ){
     pushBlockRow(array, rows, 0, 1, 2);
@@ -520,6 +593,11 @@ function pushToRows(array, blockNumber){
   
 }
 
+//function pushBlockRow(blockRowArray, store, row1, row2, row3):
+//actual push to rows/given store array from row-block
+//note: row-block is not the same as row! The first means a block
+//of 3x3 where the progression is from left to right, the second 
+//a row of 3-9 elements
 function pushBlockRow(blockRowArray, store, row1, row2, row3){
   for(var j=0;j<blockRowArray.length;++j){
     var rowElement = blockRowArray[j];
